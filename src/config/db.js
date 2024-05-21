a11e6b3020ad474bb5231f85bf510bb08e31e7df
Somesh <joshi.somesh1996@gmail.com>
2024-05-21 22:45:30 +0530

    For Deployment

--- src/config/db.js
+++ src/config/db.js
@@ -4,9 +4,9 @@ const baseUrl = process.env.MONGODB ;
 export const connectUsingMongoose = async () => {
   try {
     console.log(baseUrl)
-    await mongoose.connect(`mongodb://${baseUrl}/polling`, {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
+    await mongoose.connect(`${baseUrl}`, {
+      useNewUrlParser: true,
+       useUnifiedTopology: true,
     });
     console.log("MongoDB connected using mongoose");
   } catch (err) {
